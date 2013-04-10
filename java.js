dd59d6ef8e24acc9d180243fcdbbf84457db4ced
Vladimir Mighiu <mighiuvladimir@yahoo.coom>
2013-04-10 18:54:14 +0300

    user interface

---
+++ java.js
@@ -0,0 +1,22 @@
+$(".lii").click(function(){
+	
+	$(this).css("background","url(assets/images/poza7.png)");
+	
+});
+$(".lii a").click(function(){
+	
+
+	$(this).css("color","#949494");
+});
+
+$(".patte").hover(function(){
+
+	$(this).css("margin-top","0");
+	
+	
+	
+},function(){
+	$(this).css("width","");
+	
+	$(this).css("margin","");
+});
